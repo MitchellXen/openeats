27637c21b71ce39e9e9f78c7e8a53bc45770fc21
Xen <xen@Instance1.zone01.ams02.cldin.net>
2018-04-14 14:20:36 +0200

    Fixed wsgi script

--- web/openeats/wsgi.py
+++ web/openeats/wsgi.py
@@ -1,14 +1,33 @@
 """
-WSGI config for sharemusic project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
+exposes the WSGI callable as a module-level variable named ``application``. 
+For more information on this file, see 
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/ 
+""" 
+import os 
+import time 
+import traceback 
+import signal 
+import sys 
+ 
+from django.core.wsgi import get_wsgi_application 
 
-import os
+path = '/var/www/openeats/web'
+if path not in sys.path:
+    sys.path.append(path)
 
-from django.core.wsgi import get_wsgi_application
-
+# adjust the Python version in the line below as needed 
+sys.path.append('/home/xen/pyenv/lib/python2.7/site-packages') 
+ 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openeats.settings")
 
-application = get_wsgi_application()
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
+
+
+
